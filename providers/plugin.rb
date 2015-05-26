7bc3093178ced176e5c95fde525e8e790805b397
Richard Heelin <richardh@channelgrabber.com>
2015-05-26 15:07:35 +0100

    CGIV-5687: Can't use local variables inside resources

--- providers/plugin.rb
+++ providers/plugin.rb
@@ -8,34 +8,58 @@ def load_current_resource
 end
 
 action :install do
-  ex = execute "bin/plugin install #{@name}" do
-    user     @user
-    group    @group
-    cwd      @instance_dir
-    notifies :restart, "logstash_service[#{@instance}]", :delayed
-    not_if   "bin/plugin install --installed '^#{@name}$'", :user => @user, :group => @group, :cwd => @instance_dir
+  ls_name = @name
+  ls_instance = @instance
+  ls_basedir = @base_directory
+  ls_user = @user
+  ls_group = @group
+  ls_instance_dir = @instance_dir
+
+  ex = execute "bin/plugin install #{ls_name}" do
+    user     ls_user
+    group    ls_group
+    cwd      ls_instance_dir
+    notifies :restart, "logstash_service[#{ls_instance}]", :delayed
+    not_if   "bin/plugin install --installed '^#{ls_name}$'", :user => ls_user, :group => ls_group, :cwd => ls_instance_dir
   end
+
   new_resource.updated_by_last_action(ex.updated_by_last_action?)
 end
 
 action :update do
-  ex = execute "bin/plugin update #{@name}" do
-    user     @user
-    group    @group
-    cwd      @instance_dir
-    notifies :restart, "logstash_service[#{@instance}]", :delayed
-    only_if   "bin/plugin install --installed '^#{@name}$'", :user => @user, :group => @group, :cwd => @instance_dir
+  ls_name = @name
+  ls_instance = @instance
+  ls_basedir = @base_directory
+  ls_user = @user
+  ls_group = @group
+  ls_instance_dir = @instance_dir
+
+  ex = execute "bin/plugin update #{ls_name}" do
+    user     ls_user
+    group    ls_group
+    cwd      ls_instance_dir
+    notifies :restart, "logstash_service[#{ls_instance}]", :delayed
+    only_if  "bin/plugin install --installed '^#{ls_name}$'", :user => ls_user, :group => ls_group, :cwd => ls_instance_dir
   end
+
   new_resource.updated_by_last_action(ex.updated_by_last_action?)
 end
 
 action :remove do
-  ex = execute "bin/plugin uninstall #{@name}" do
-    user     @user
-    group    @group
-    cwd      @instance_dir
-    notifies :restart, "logstash_service[#{@instance}]", :delayed
-    only_if  "bin/plugin install --installed '^#{@name}$'", :user => @user, :group => @group, :cwd => @instance_dir
+  ls_name = @name
+  ls_instance = @instance
+  ls_basedir = @base_directory
+  ls_user = @user
+  ls_group = @group
+  ls_instance_dir = @instance_dir
+
+  ex = execute "bin/plugin uninstall #{ls_name}" do
+    user     ls_user
+    group    ls_group
+    cwd      ls_instance_dir
+    notifies :restart, "logstash_service[#{ls_instance}]", :delayed
+    only_if  "bin/plugin install --installed '^#{ls_name}$'", :user => ls_user, :group => ls_group, :cwd => ls_instance_dir
   end
+
   new_resource.updated_by_last_action(ex.updated_by_last_action?)
 end
